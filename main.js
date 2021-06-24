b03e24b65a7bdd145c947fee8ded99ef68e7ab6b
OneNylxus <onenylxus@gmail.com>
2021-06-24 18:59:42 +0800

    Header and navigation menu

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
-// Console log
-console.log('Nicholas Ng');
+// Snippet function for HTML elements
+function qs(sel) {
+  return document.querySelector(sel);
+}
+
+function qsa(sel) {
+  return document.querySelectorAll(sel);
+}
+
+/* ------------------------ division ------------------------ */
+
+// Show menu
+if (qs('.nav-toggle')) {
+  qs('.nav-toggle').addEventListener('click', () => {
+    qs('.nav-menu').classList.add('show-menu');
+  });
+}
+
+// Hide menu
+if (qs('.nav-close')) {
+  qs('.nav-close').addEventListener('click', () => {
+    qs('.nav-menu').classList.remove('show-menu');
+  });
+}
+
+/* ------------------------ division ------------------------ */
+
+// Remove menu for mobile
+qsa('.nav-link').forEach((link) => link.addEventListener('click', () => {
+  qs('.nav-menu').classList.remove('show-menu');
+}));
