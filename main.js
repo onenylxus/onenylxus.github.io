d34797bdd856a246918609703756282fc7319472
OneNylxus <onenylxus@gmail.com>
2021-08-11 14:57:39 +0800

    About and skills section

--- main.js
+++ main.js
@@ -23,9 +23,19 @@ if (qs('.nav-close')) {
   });
 }
 
-/* ------------------------ division ------------------------ */
-
 // Remove menu for mobile
 qsa('.nav-link').forEach((link) => link.addEventListener('click', () => {
   qs('.nav-menu').classList.remove('show-menu');
+}));
+
+/* ------------------------ division ------------------------ */
+
+// Toggle skills
+qsa('.skills-header').forEach((header) => header.addEventListener('click', () => {
+  qsa('.skills-content').forEach((content) => {
+    content.className = "skills-content skills-close";
+  });
+  if (header.parentNode.className === 'skills-content skills-close') {
+    header.parentNode.className = 'skills-content skills-open';
+  }
 }));
